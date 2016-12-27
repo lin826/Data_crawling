db7d03b7437a7a4267286726906d23c6f798d2db
lin826 <liniju826@gmail.com>
2016-12-27 21:03:50 +0800

    Modify delay

--- Craw_hashtag_12_26.py
+++ Craw_hashtag_12_26.py
@@ -26,18 +26,20 @@ with open(input_file, 'rU') as file:
         for row in csv.reader(file, delimiter=','):
             print(row[0])
             driver_get.get('https://www.instagram.com/'+row[0])
-            for i in range(0,3):
+            number = 0
+            for i in range(0,3): # prevent for webpage loading
                 try:
                     driver_get.find_element_by_class_name("_oidfu").click() # more button
+                    number = driver_get.find_element_by_class_name("_bkw5z").text # number of posts
+                    number = int(number.replace(',',''))
                     break
                 except NoSuchElementException:
                     print('NoSuchElementException in driver_get for button more')
                     time.sleep(1)
-            number = driver_get.find_element_by_class_name("_bkw5z").text # number of posts
-            number = int(number.replace(',',''))
             previous = 0
             while(previous<number):
-                driver_get.execute_script("window.scrollTo(0, document.body.scrollHeight/);")
+                driver_get.execute_script("window.scrollTo(0, document.body.scrollHeight/2);")
+                time.sleep(1)
                 driver_get.execute_script("window.scrollTo(0, document.body.scrollHeight);")
                 e = driver_get.find_elements_by_class_name("_icyx7") # each item
                 for index in range(previous,len(e)):
@@ -47,3 +49,5 @@ with open(input_file, 'rU') as file:
                     fieldnames[2]:e[index].get_attribute("src"),
                     fieldnames[3]:(e[index].get_attribute("alt")).encode("utf-8")})
                 previous = len(e)
+                if(previous==0):
+                    break
